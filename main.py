6d1df11e7df0b94df2897b3078eb3d52408ef1f3
PwnHash <106488583+PwnHash@users.noreply.github.com>
2022-12-27 07:31:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ auth_token = 'YOUR_TWILIO_AUTH_TOKEN'
 # Your Twilio phone number (in E.164 format, e.g. +15551234567)
 from_number = 'YOUR_TWILIO_PHONE_NUMBER'
 
-# The phone number you want to send the message to (in E.164 format)
-to_number = 'RECIPIENT_PHONE_NUMBER'
-
 # The custom sender ID you want to use (must be approved by Twilio)
 custom_sender_id = 'YOUR_CUSTOM_SENDER_ID'
 
 # The message you want to send
 message = 'This is a test message'
 
+# The list of phone numbers you want to send the message to (in E.164 format)
+phone_numbers = ['RECIPIENT_PHONE_NUMBER_1', 'RECIPIENT_PHONE_NUMBER_2', ...]
+
 # Create a Twilio client
 client = Client(account_sid, auth_token)
 
-# Send the message
-message = client.messages.create(
-    body=message,
-    from_=custom_sender_id,
-    to=to_number
-)
-
-print(f'Message sent with SID {message.sid}')
+# Send the message to each phone number in the list
+for phone_number in phone_numbers:
+    message = client.messages.create(
+        body=message,
+        from_=custom_sender_id,
+        to=phone_number
+    )
+    print(f'Message sent to {phone_number} with SID {message.sid}')
